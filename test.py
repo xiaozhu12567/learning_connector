2731edbac6d882f76facadcfa888fdd493fadf0b
xiaozhu12567 <470793114@qq.com>
2016-09-15 05:47:42 +0800

    New

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-print hehe
+#! /usr/bin/env python
+# coding:utf-8
+
+import MySQLdb
+
+db_config = {
+	'host':'localhost',
+	'port':3306,
+	'user':'zhu',
+	'passwd':'123',
+	'db':'test1'
+}
+sql1 = '''
+	create table `test_connector`(
+	`id` int PRIMARY KEY auto_increment,
+	`name` varchar(10) NOT NULL
+)
+'''
+sql2 = '''
+	SHOW TABLES
+'''
+conn = MySQLdb.connect(**db_config)
+cur = conn.cursor()
+cur.execute(sql1)
+cur.execute(sql2)
+for i in cur:
+	print i
+cur.close()
+conn.close()
